1ed8ff900f7bde5bdec98e8f8c3e1b0d1fae54b9
Domonique Taylor <taylord651@gmail.com>
2019-09-02 13:28:48 -0500

    Reconfigured remove method to remove cheese based on id, refactored processAddCheeseForm and add.html to use model binding

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,14 +1,15 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Domonique Taylor
@@ -18,14 +19,12 @@ import java.util.HashMap;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
-
 
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
 
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
@@ -38,9 +37,17 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description) {
-        Cheese newCheese = new Cheese(cheeseName, description);
-        cheeses.add(newCheese);
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+
+        /* Code that is being implicitly run for us by Spring Boot
+        *
+        * Cheese newCheese = new Cheese();  //Call the default constructor
+        * newCheese.setName(Request.getParameter("name")); //Go to request, look for a parameter that corresponds to the name of the field that we are trying to set, and call that
+        * newCheeses.setDescription(Request.getParameter("description"));
+        *
+        * */
+        
+        CheeseData.add(newCheese);
 
         // Redirect to /cheese
         return "redirect:";
@@ -50,16 +57,18 @@ public class CheeseController {
     public String removeCheesesForm(Model model) {
         model.addAttribute("title", "Remove Cheese");
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
 
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String removeCheeseForm(@RequestParam ArrayList<String> cheese) {
-        for (String aCheese : cheese) {
-            cheeses.remove(aCheese);
+    public String removeCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
+
         // Redirect to /cheese
         return "redirect:";
     }
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -7,9 +7,30 @@ public class Cheese {
     private String name;
     private String description;
 
+
+    // Unique number to identify each object created from the cheese class
+    // Allow us to delete a cheese object even if it share its name with another object
+    private int cheeseId;
+
+    private static int nextId = 1;
+
     public Cheese(String name, String description) {
+        this();
         this.name = name;
         this.description = description;
+    }
+
+    public Cheese() {
+        cheeseId = nextId;
+        nextId++;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
     }
 
     public String getName() {
