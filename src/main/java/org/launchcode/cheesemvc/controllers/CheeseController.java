c5bf3aa17da2f1e19f7dc8d74e0c4cca290c5c20
Domonique Taylor <taylord651@gmail.com>
2019-09-02 09:28:39 -0500

    Added cheese model to store data in a place separate from the controller.

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ import java.util.HashMap;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
 
     // Request path: /cheese
@@ -38,8 +39,27 @@ public class CheeseController {
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description) {
-        cheeses.put(cheeseName, description);
+        Cheese newCheese = new Cheese(cheeseName, description);
+        cheeses.add(newCheese);
 
+        // Redirect to /cheese
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String removeCheesesForm(Model model) {
+        model.addAttribute("title", "Remove Cheese");
+
+        model.addAttribute("cheeses", cheeses);
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String removeCheeseForm(@RequestParam ArrayList<String> cheese) {
+        for (String aCheese : cheese) {
+            cheeses.remove(aCheese);
+        }
         // Redirect to /cheese
         return "redirect:";
     }
---
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,30 @@
+package org.launchcode.cheesemvc.models;
+
+
+//Replaces string item in controller to store data
+public class Cheese {
+
+    private String name;
+    private String description;
+
+    public Cheese(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
